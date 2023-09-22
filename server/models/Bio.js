4b8a8bb592dcb8cb2af3775c68fa5c9194ef1cd7
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-09-22 13:51:07 -0600

    edit to bioSchema

--- server/models/Bio.js
+++ server/models/Bio.js
@@ -4,15 +4,9 @@ const { Schema } = require('mongoose');
 const bioSchema = new Schema({
   skills: {
     type: String,
-    required: true,
-    unique: true,
-    trim: true,
   },
   location: {
     type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
   },
   userDescription: {
     type: String,
