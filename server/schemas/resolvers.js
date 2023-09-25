d8207ea77f5e0a73b4c0c5357e8fe99eedfb241e
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-09-25 09:12:17 -0600

    added resolvers for Users, user, me, mutation for addUser and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,48 @@
 // resolvers
+const { AuthenticationError } = require('apollo-server-express');
 const { User, JobPost, Application } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate('jobsAppliedTo').populate('postedJobs').populate('applications');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('jobsAppliedTo').populate('postedJobs').populate('applications');
+    },
+    me: async (parent, args, context) => {
+      // if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('jobsAppliedTo').populate('postedJobs').populate('applications');
+      //},
+      // throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
