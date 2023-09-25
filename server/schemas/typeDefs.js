8fbdc02741e9dae900141cffbf8dce0ea0124499
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-09-25 09:18:17 -0600

    added typeDefs for users, user, me, adduser, login

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,34 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: String
+  bio: [Bio]!
+  resume: File
+  jobsAppliedTo: [JobPost]!
+  postedJobs: [JobPost]!
+  applications: [Application]!
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  users: [User]
+  user(username: String!): User
+  me: User
+}
+
+type Mutation {
+  addUser(username: String!, email: String!, password: String!): Auth
+  login(email: String!, password: String!): Auth
+}
+
 `;
 
 module.exports = typeDefs;
