f8bf419f8d4ccd095b37350132496dac0414f861
kyungkwon01 <kyungkwon01@gmail.com>
2023-09-25 19:18:26 -0600

    auth.js file in frontend and add mutations and queries file

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,3 +1,37 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, JobPost, Bio, Application } = require('../models');
-const { signToken } = require('../utils/auth');
+import decode from 'jwt-decode';
+
+class AuthService {
+	getProfile() {
+		return decode(this.getToken());
+	}
+
+	loggedIn() {
+		const token = this.getToken();
+		return token && !this.isTokenExpired(token) ? true : false;
+	}
+
+	isTokenExpired(token) {
+		const decoded = decode(token);
+		if (decoded.exp < Date.now() / 1000) {
+			localStorage.removeItem('id_token');
+			return true;
+		}
+		return false;
+	}
+
+	getToken() {
+		return localStorage.getItem('id_token');
+	}
+
+	login(idToken) {
+		localStorage.setItem('id_token', idToken);
+		window.location.assign('/');
+	}
+
+	logout() {
+		localStorage.removeItem('id_token');
+		window.location.reload();
+	}
+}
+
+export default new AuthService();
