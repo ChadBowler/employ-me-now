5bbc8806c1342795049c628c902fd466126b59be
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-09-25 19:25:43 -0600

    added client side mutations for login, add user, update profile, post job

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const UPDATE_PROFILE = gql`
+  mutation updateProfile($userId: ID!, $skills: String!, $location: String!, $userDescription: String!, $resume: String!) {
+    updateProfile(userId: $userId, skills: $skills, location: $location, userDescription: $userDescription, resume: $resume) {
+      user {
+        _id
+        username
+        bio {
+          skills
+          location
+          userDescription
+        }
+        resume
+      }
+`;
+
+export const POST_JOB = gql`
+  mutation postJob($userId: ID!, $title: String!, $company: String!, $salary: Int!, $description: String!) {
+    postJob(userId: $userId, title: $title, company: $company, salary: $salary, description: $description) {
+      _id
+      title
+      company
+      salary
+      description
+      author {
+        _id
+        username
+      }
+    }
+  }
+`;
