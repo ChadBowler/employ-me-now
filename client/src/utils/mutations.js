6bd19b18943c305cf2aa031027c09643f27ab4c2
kyungkwon01 <kyungkwon01@gmail.com>
2023-09-25 20:35:51 -0600

    frontend mutations and queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,27 +1,27 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
 `;
 
 export const UPDATE_PROFILE = gql`
@@ -40,17 +40,46 @@ export const UPDATE_PROFILE = gql`
 `;
 
 export const POST_JOB = gql`
-  mutation postJob($userId: ID!, $title: String!, $company: String!, $salary: Int!, $description: String!) {
-    postJob(userId: $userId, title: $title, company: $company, salary: $salary, description: $description) {
+	mutation postJob(
+		$userId: ID!
+		$title: String!
+		$company: String!
+		$salary: Int!
+		$description: String!
+	) {
+		postJob(
+			userId: $userId
+			title: $title
+			company: $company
+			salary: $salary
+			description: $description
+		) {
+			_id
+			title
+			company
+			salary
+			description
+			author {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+export const APPLY_TO_JOB = gql`
+  mutation applyToJob($userId: ID!
+    $resume: String!
+    $jobId: ID!
+  ) {
+    applyToJob(
+      userId: $userId
+      resume: $resume
+      jobId: $jobId
+    ) {
       _id
-      title
-      company
-      salary
-      description
-      author {
-        _id
-        username
-      }
+      resume
+      jobId
     }
   }
-`;
+}`;
