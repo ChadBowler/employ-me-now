cd4bf10ed7e0f54818bdde0e8e5ba5816e284d62
Maridon Romney <mkromney@gmail.com>
2023-09-27 15:47:41 -0600

    added message sent for contact form, still unable to force API to use required fields messages.

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,39 +1,70 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
-//npm i @emailjs/browser
-
-//SYTLES HERE
+// Styles go here
 
 export const ContactForm = () => {
   const form = useRef();
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [messageError, setMessageError] = useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(
-      "service_umymdxc", 
-      "template_0xqzqfo", 
-      form.current, 
-      "UXMrhYP-dSOk916Oc"
-      )
+    // Resets previous error messages. //
+    setNameError('');
+    setEmailError('');
+    setMessageError('');
+
+    const { user_name, user_email, message } = e.target.elements;
+
+    if (!user_name.value) {
+      setNameError('Name is required');
+      return;
+    }
+
+    if (!user_email.value.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i)) {
+      setEmailError('Please enter a valid email');
+      return;
+    }
+
+    if (!message.value || message.value.length < 5 || message.value.length > 500) {
+      setMessageError('Message must be between 5 and 500 characters');
+      return;
+    }
+
+    emailjs
+      .sendForm('service_umymdxc', 'template_0xqzqfo', form.current, 'UXMrhYP-dSOk916Oc')
       .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
+        console.log(result.text);
+        setSuccessMessage('Message has been sent!');
+      })
+      .catch((error) => {
+        console.log(error.text);
       });
   };
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
+      <label>
+        Name
+        <input type="text" name="user_name" required />
+        {nameError && <div style={{ color: 'red' }}>{nameError}</div>}
+      </label>
+      <label>
+        Email
+        <input type="email" name="user_email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" />
+        {emailError && <div style={{ color: 'red' }}>{emailError}</div>}
+      </label>
+      <label>
+        Message
+        <textarea name="message" required minLength={5} maxLength={500} />
+        {messageError && <div style={{ color: 'red' }}>{messageError}</div>}
+      </label>
       <input type="submit" value="Send" />
+      {successMessage && <div>{successMessage}</div>}
     </form>
   );
 };
-
